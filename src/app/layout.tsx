1aeb0ba9152ca56a5ca4a83bb7d19abe573c151b
Mehedi Hasan <mehedibuet73@gmail.com>
2025-01-05 05:42:01 +0600

    addded favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/public/fav.webp" type="image/x-icon" />
+      </head>
       <body
         className={twMerge(
           inter.className,
